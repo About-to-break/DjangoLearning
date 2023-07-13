921156e53b27d2dead50f18d537ce558f5b8ee99
Eugene ABT <111evgenos111@gmail.com>
2023-07-13 16:15:06 +0300

    Cards are now dynamic -> Now title and promotions in products.html relies on code in views.py

--- products/views.py
+++ products/views.py
@@ -12,5 +12,20 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - каталог',
+        'products': [
+            {'image': "/static/vendor/img/products/Adidas-hoodie.png",
+             'name': "Худи черного цвета с монограммами adidas Originals",
+             'price': 6090,
+             'description': "Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни."
+             },
+            {'image': "/static/vendor/img/products/Black-Dr-Martens-shoes.png",
+             'name': "Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex",
+             'price': 13590,
+             'description': "Гладкий кожаный верх. Натуральный материал."
+             }
+        ]
+    }
+    return render(request, 'products/products.html', context)
 
